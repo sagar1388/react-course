d9fe59c50b133ba8e9887baf972462afcf49710a
sagar1388 <sagarmane1388@gmail.com>
2023-10-14 22:55:10 +0530

    Functional Component Changes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", class: "heading-class" },
@@ -56,13 +59,52 @@
 // </div>
 //</div>
 
-const div = React.createElement(
-    "div",
-    {},
-    [React.createElement("div", {class: "nested-container"}, [React.createElement("h1", {}, "Big Heading"), React.createElement("h2", {}, "Small Heading")]),
-    React.createElement("div", {class: "nested-container2"}, [React.createElement("p", {}, "Paragraph"), React.createElement("span", {}, "Span")])]
-  );
+// const div = React.createElement("div", {}, [
+//   React.createElement("div", { class: "nested-container" }, [
+//     React.createElement("h1", {}, "Big Heading"),
+//     React.createElement("h2", {}, "Small Heading"),
+//   ]),
+//   React.createElement("div", { class: "nested-container2" }, [
+//     React.createElement("p", {}, "Paragraph"),
+//     React.createElement("span", {}, "Span"),
+//   ]),
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById("container"));
+
+// root.render(div);
+
+//JSX
+  // const heading = React.createElement(
+  //   "h1",
+  //   { id: "heading", class: "heading-class" },
+  //   "Hello World from React!!!"
+  // );
   
+// Creating above code in JSX
+
+  //const jsxHeading = <h1 id="heading">Hello World from JSX!!</h1>
+
+  const jsxHeading = (
+    <div>
+  <h1 id="heading">Hello World from JSX!!</h1>
+  <h2 id="heading2">Hello World from JSX 2!!</h2>
+  </div>
+  );
+
+  // const Heading = () => {
+  //   return <h1>This is a functional component.</h1>
+  // }
+
+  // const Heading = () => <h1>This is a functional component.</h1>;
+
+  const Heading = () => {
+    return (<div>
+      <h1>This is a functional component.</h1>
+      </div>
+    )
+  };
+
   const root = ReactDOM.createRoot(document.getElementById("container"));
   
-  root.render(div);
+  root.render(<Heading />);
